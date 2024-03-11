a99e25f69dc984af61bd83b3dd4df8e510979714
Paul <paul.cornille-avot@supinfo.com>
2024-03-11 14:40:14 +0100

    hit box ajouter

--- jeu_staryinsh.py
+++ jeu_staryinsh.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-
+from math import*
 pygame.init()
 
 def jeu():
@@ -12,15 +12,60 @@ def jeu():
     fond_ecran = pygame.transform.scale(fond_ecran, (largeur_ecran, hauteur_ecran))
 
     en_cours = True
-    
+    ecran.blit(fond_ecran, (0, 0))
     while en_cours:
+        
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
                 from menu_staryinsh import menu
                 menu()
-            
-                            
-        ecran.blit(fond_ecran, (0, 0))
+            for i in range(4):
+                Hexagon(723+(i*75), 220-(i*43), 30).draw(ecran)
+            for i in range(7):
+                Hexagon(647+(i*75), 345-(i*43), 30).draw(ecran)
+            for i in range(8):
+                Hexagon(647+(i*75), 435-(i*43), 30).draw(ecran)
+            for i in range(9):
+                Hexagon(647+(i*75), 520-(i*43), 30).draw(ecran)                  
+            for i in range(10):
+                Hexagon(647+(i*75), 605-(i*43), 30).draw(ecran) 
+            for i in range(9):
+                Hexagon(723+(i*75), 650-(i*43), 30).draw(ecran) 
+            for i in range(10):
+                Hexagon(723+(i*75), 737-(i*43), 30).draw(ecran) 
+            for i in range(9):
+                Hexagon(797+(i*75), 780-(i*43), 30).draw(ecran) 
+            for i in range(8):
+                Hexagon(873+(i*75), 825-(i*43), 30).draw(ecran) 
+            for i in range(7):
+                Hexagon(950+(i*75), 867-(i*43), 30).draw(ecran) 
+            for i in range(4):
+                Hexagon(1100+(i*75), 867-(i*43), 30).draw(ecran)
+                    
         pygame.display.flip()
-
     pygame.quit()
+
+class Hexagon:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.points = self.calculate_points()
+
+    def calculate_points(self):
+        points = []
+        for i in range(6):
+            angle_deg = 60 * i
+            angle_rad = pi / 180 * angle_deg
+            points.append((self.x + self.size * cos(angle_rad),self.y + self.size * sin(angle_rad)))
+        return points
+
+    def draw(self, surface, color=(255, 255, 255)):
+        pygame.draw.polygon(surface, color, self.points)
+
+    def is_point_inside(self, point):
+        x, y = point
+        return pygame.Rect(self.x - self.size, self.y - self.size, 2*self.size, 2*self.size).collidepoint(x, y)
+     
+    
+jeu()
--- menu_staryinsh.py
+++ menu_staryinsh.py
@@ -7,49 +7,57 @@ import parametre_staryinsh as parametre
 pygame.init()
 
 def menu():
+    #initialisation de la fenetre
     largeur_ecran, hauteur_ecran = pygame.display.Info().current_w, pygame.display.Info().current_h
     ecran = pygame.display.set_mode((largeur_ecran, hauteur_ecran))
     pygame.display.set_caption("Menu")
 
+    #chargement de l'image de fond
     fond_ecran = pygame.image.load('fond.jpeg')
     fond_ecran = pygame.transform.scale(fond_ecran, (largeur_ecran, hauteur_ecran))
     
     ratio_largeur = largeur_ecran / 2048
     ratio_hauteur = hauteur_ecran / 1152
     
+    #charegement des images
     bouton_demarrer_image = pygame.image.load('jouer.jpg')
     bouton_regle_image = pygame.image.load('regle.jpg')
     bouton_parametre_image = pygame.image.load('parametre.jpg')
     
+    #redimensionnement des images
     bouton_demarrer_image = pygame.transform.scale(bouton_demarrer_image, (800*ratio_largeur, 700*ratio_hauteur))
     bouton_regle_image = pygame.transform.scale(bouton_regle_image, (800*ratio_largeur, 350*ratio_hauteur))
     bouton_parametre_image = pygame.transform.scale(bouton_parametre_image, (800*ratio_largeur, 350*ratio_hauteur))
 
+    #initialisation des variables
     survol_demarrer = False
     survol_regle = False
     survol_parametre = False
     en_cours = True
     
+    # s'occupe des evenements
     while en_cours:
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
                 en_cours = False
             elif event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if 1096*ratio_largeur <= event.pos[0] <= 1846*ratio_largeur and 200*ratio_hauteur <= event.pos[1] <= 550*ratio_hauteur:
+                    if 1046*ratio_largeur <= event.pos[0] <= 1796*ratio_largeur and 200*ratio_hauteur <= event.pos[1] <= 550*ratio_hauteur:
                         regle.carrousel()
-                    if 300*ratio_largeur <= event.pos[0] <= 1100*ratio_largeur and 200*ratio_hauteur <= event.pos[1] <= 900*ratio_hauteur:
+                    if 250*ratio_largeur <= event.pos[0] <= 1050*ratio_largeur and 200*ratio_hauteur <= event.pos[1] <= 900*ratio_hauteur:
                         jeu.jeu()
-                    if 1096*ratio_largeur <= event.pos[0] <= 1696*ratio_largeur and 549*ratio_hauteur <= event.pos[1] <= 899*ratio_hauteur:
+                    if 1046*ratio_largeur <= event.pos[0] <= 1646*ratio_largeur and 549*ratio_hauteur <= event.pos[1] <= 899*ratio_hauteur:
                         parametre.parametre()
             elif event.type == MOUSEMOTION:    
                 x, y = event.pos
-                survol_demarrer = 300*ratio_largeur <= x <= 300*ratio_largeur + bouton_demarrer_image.get_width() and 200*ratio_hauteur <= y <= 200*ratio_hauteur + bouton_demarrer_image.get_height()
-                survol_regle = 1096*ratio_largeur <= x <= 1096*ratio_largeur + bouton_regle_image.get_width() and 200*ratio_hauteur <= y <= 200*ratio_hauteur + bouton_regle_image.get_height()
-                survol_parametre = 1096*ratio_largeur <= x <= 1096*ratio_largeur + bouton_parametre_image.get_width() and 549*ratio_hauteur <= y <= 549*ratio_hauteur + bouton_parametre_image.get_height()
+                survol_demarrer = 250*ratio_largeur <= x <= 250*ratio_largeur + bouton_demarrer_image.get_width() and 200*ratio_hauteur <= y <= 200*ratio_hauteur + bouton_demarrer_image.get_height()
+                survol_regle = 1046*ratio_largeur <= x <= 1046*ratio_largeur + bouton_regle_image.get_width() and 200*ratio_hauteur <= y <= 200*ratio_hauteur + bouton_regle_image.get_height()
+                survol_parametre = 1046*ratio_largeur <= x <= 1046*ratio_largeur + bouton_parametre_image.get_width() and 549*ratio_hauteur <= y <= 549*ratio_hauteur + bouton_parametre_image.get_height()
 
         ecran.blit(fond_ecran, (0, 0))
         
+        
+        # Affichage des boutons pour les placer au bon endroit sur l'écran
         if survol_demarrer:
             ecran.blit(pygame.transform.scale(bouton_demarrer_image, (bouton_demarrer_image.get_width() + 15, bouton_demarrer_image.get_height() + 15)), (245*ratio_largeur, 195*ratio_hauteur))
         else:
