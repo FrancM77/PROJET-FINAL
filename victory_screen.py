d24ad475d0be030c22fb39570279d795aa24dc7b
Paul <paul.cornille-avot@supinfo.com>
2024-05-15 14:18:21 +0200

    sound

--- victory_screen.py
+++ victory_screen.py
@@ -46,8 +46,8 @@ def victory_screen(winner,mode,type_game):
                     if 919*width_ratio <= event.pos[0] <= 1498*width_ratio and 853*height_ratio <= event.pos[1] <= 960*height_ratio:
                         menu.play_button_sound()
                         pygame.mixer.music.stop()
-                        sound = pygame.mixer.Sound(f'sounds/game_over.mp3')
-                        sound.play()
+                        pygame.mixer.music.Sound('sounds/menu.mp3')
+                        pygame.mixer.music.play(-1)
                         menu.menu()
             elif event.type == MOUSEMOTION:
                 x, y = event.pos
